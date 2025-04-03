b6f64db02eb10ec94c13cf483b90b63f1d4f565f
NitheeshSamadhi <nitheesh8186@gmail.com>
2025-04-03 16:19:48 +0530

    implemented next function in routing

--- src/app.js
+++ src/app.js
@@ -2,31 +2,31 @@ const express = require("express");
 const app = express();
 
 
-
-app.use("/home",(req,res)=>{
-    res.send("this is home page");
-     
-})
-
-app.get("/user",(req,res)=>{
-    res.send({"name":"nitheesh","age":21})
-})
-
-app.post("/user",(req,res)=>{
-    res.send("data sended to the database successfully")
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("data deleted successfully")
-})
-
-app.put("/user",(req,res)=>{
-    res.send("data updated successfully")
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("data patched successfully")
-})
+app.use(
+"/user",
+(req,res,next)=>{
+    console.log("this is first routing");
+    next();
+    res.send("this 1st routing");
+    
+},
+(req,res,next)=>{
+    console.log("this is second routing");
+    next();
+    res.send("this is 2nd routing");
+    
+},
+(req,res,next)=>{
+    console.log("this is third route")
+    next()
+    res.send("this is 3rd routing")
+},
+(req,res,next)=>{
+    console.log("this is fourth routing");
+    next()
+    // res.send("this is 4th routing")
+}
+)
 
 app.listen(3000,()=>{
     console.log("app listening on port number 3000")
